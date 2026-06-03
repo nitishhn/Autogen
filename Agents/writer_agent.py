ad924c0f33726e40bb32a450cb425ffe06772aa9
nitish <nitishnalatawad111@gmail.com>
2026-06-03 12:11:26 +0530

    made more

--- Agents/writer_agent.py
+++ Agents/writer_agent.py
@@ -3,14 +3,5 @@ import autogen
 from dotenv import load_dotenv
 load_dotenv()
 
-config_list = [
-    {
-        "model": os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
-        "api_type": "azure",
-        "base_url": os.getenv("AZURE_OPENAI_ENDPOINT"),
-        "api_key": os.getenv("AZURE_OPENAI_KEY"),
-        "api_version": "2025-01-01-preview",
-    }
-]
 
 
